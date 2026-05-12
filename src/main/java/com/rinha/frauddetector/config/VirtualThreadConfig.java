a3b7db1affbeab54820a8475d80fe6da9a69ceae
Guilherme Gules Moreira <guilhermegules@gmail.com>
2026-05-12 08:58:40 -0300

    feat: add virtual threads config

---
+++ src/main/java/com/rinha/frauddetector/config/VirtualThreadConfig.java
@@ -0,0 +1,16 @@
+package com.rinha.frauddetector.config;
+
+import org.springframework.boot.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+
+@Configuration
+public class VirtualThreadConfig {
+
+  @Bean
+  public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+    return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+  }
+}
